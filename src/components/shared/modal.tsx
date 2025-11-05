0dcbaa31a0feef381aa155e18727756cdf767cd2
Amir Ridwan <amirridwan.dev@gmail.com>
2025-11-06 02:25:24 +0800

    feat: init shared modal

---
+++ src/components/shared/modal.tsx
@@ -0,0 +1,23 @@
+export default function CreateModal({
+  isOpen,
+  onClose,
+  children,
+}: Readonly<{
+  isOpen: boolean;
+  onClose: Function;
+  children: React.ReactNode;
+}>) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex justify-center items-center">
+      <div
+        className="absolute bg-gray-800 opacity-50 h-screen w-screen justify-center items-center z-10"
+        onClick={() => onClose()}
+      />
+      <div className="flex flex-col gap-5 absolute bg-white rounded-md p-8 min-h-[10%] w-[30%] z-20">
+        {children}
+      </div>
+    </div>
+  );
+}
